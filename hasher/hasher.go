d3a881e89c79cffe4c0570d6b5850250445d7b7c
Peter Nagy <peter.nagy@recart.com>
2021-02-09 21:39:45 +0100

    :sparkles: add hasher

---
+++ hasher/hasher.go
@@ -0,0 +1,45 @@
+package hasher
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"math/big"
+)
+
+// Hasher enables to calculate an "aggregated" hash of multiple io.Reader inputs
+type Hasher struct {
+	hash *big.Int
+}
+
+// New creates a new Hasher
+func New() *Hasher {
+	return &Hasher{
+		hash: &big.Int{},
+	}
+}
+
+// HashFile returns md5 hash byte array for an io.Reader
+func HashFile(handler io.Reader) ([]byte, error) {
+	hash := md5.New()
+	if _, err := io.Copy(hash, handler); err != nil {
+		return nil, err
+	}
+	return hash.Sum(nil), nil
+}
+
+// constructs a single hash representing multiple files
+// by combining (xor) each individual hashes
+func (h *Hasher) addHash(handler io.Reader) error {
+	bytes, err := HashFile(handler)
+	if err != nil {
+		return err
+	}
+	h.hash = h.hash.Xor(h.hash, (&big.Int{}).SetBytes(bytes))
+	return nil
+}
+
+// MD5 returns calculated md5 hash
+func (h *Hasher) MD5() string {
+	return fmt.Sprintf("%x", h.hash)
+}
